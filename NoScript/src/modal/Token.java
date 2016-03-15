417aa92a8b8a938e298bd68a514b316f4289df4b
sarosh-madara <saroshmadara@gmail.com>
2016-03-15 12:10:33 +0500

    basic structure Created

---
+++ NoScript/src/modal/Token.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modal;
+
+/**
+ *
+ * @author saroshmadara
+ */
+public class Token {
+    
+    public int ln;
+    public String classpart;
+    public char[] valuePart;
+
+    public int getLn() {
+        return ln;
+    }
+
+    public void setLn(int ln) {
+        this.ln = ln;
+    }
+
+    public String getClasspart() {
+        return classpart;
+    }
+
+    public void setClasspart(String classpart) {
+        this.classpart = classpart;
+    }
+
+    public char[] getValuePart() {
+        return valuePart;
+    }
+
+    public void setValuePart(char[] valuePart) {
+        this.valuePart = valuePart;
+    }
+    
+    
+    
+}
---
+++ NoScript/src/noscript/NoScript.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package noscript;
+
+/**
+ *
+ * @author saroshmadara
+ */
+public class NoScript {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        String[] keywords = {"int","float","char","string","double","long"};
+        
+        String streamReader = "public static void main(){ \n int a = 1; \n }";
+        
+        
+    }
+    
+}
